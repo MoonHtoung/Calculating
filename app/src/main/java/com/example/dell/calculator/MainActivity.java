e2fdf2d63cad46f6f44bc29d2eb96a571446ea6d
MoonHtoung <briony.mt@gmail.com>
2019-06-13 21:04:41 +0630

    first commit

--- app/src/main/java/com/example/dell/calculator/MainActivity.java
+++ app/src/main/java/com/example/dell/calculator/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.dell.calculator;
 
+import android.app.Notification;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,9 +12,23 @@ import android.widget.EditText;
 
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-   EditText input1;
-   Button btn_1,btn_2,btn_3;
+
+import java.lang.reflect.Array;
+
+
+public class MainActivity extends AppCompatActivity {
+   EditText input;
+   Button btn_1,btn_2,btn_3,btn_4,btn_5,btn_6,btn_7,btn_8,btn_9,btn_0,btn_add,btn_sub,btn_r,btn_reset;
+
+    String Action;
+
+
+    Float prev_result = 0f;
+    Float final_result = 0f;
+
+    String final_number = "";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,18 +36,168 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
 
-        input1 = (EditText) findViewById(R.id.input1);
-        input1.getText();
+        input = (EditText) findViewById(R.id.input);
+        btn_0 = (Button) findViewById(R.id.btn_0);
+        btn_1 = (Button) findViewById(R.id.btn_1);
+        btn_2 = (Button) findViewById(R.id.btn_2);
+        btn_3 = (Button) findViewById(R.id.btn_3);
+        btn_4 = (Button) findViewById(R.id.btn_4);
+        btn_5 = (Button) findViewById(R.id.btn_5);
+        btn_6 = (Button) findViewById(R.id.btn_6);
+        btn_7 = (Button) findViewById(R.id.btn_7);
+        btn_8 = (Button) findViewById(R.id.btn_8);
+        btn_9 = (Button) findViewById(R.id.btn_9);
+        btn_add = (Button) findViewById(R.id.btn_add);
+        btn_r = (Button) findViewById(R.id.btn_r);
+        btn_sub = (Button) findViewById(R.id.btn_sub);
+        btn_reset = (Button) findViewById(R.id.btn_reset);
+
+        input.setGravity(View.FOCUS_LEFT);
+        input.setCursorVisible(false);
 
 
+        btn_0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                input.setText(input.getText() + "0");
+                final_number = input.getText().toString() + "0";
+            }
+        });
+
+        btn_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                input.setText(input.getText() + "1");
+                final_number = input.getText().toString() + "1";
+
+            }
+        });
+
+        btn_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                input.setText(input.getText() + "2");
+                final_number = input.getText().toString() + "2";
+            }
+        });
+
+        btn_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                input.setText(input.getText() + "3");
+                final_number = input.getText().toString() + "3";
+            }
+        });
+
+        btn_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                input.setText(input.getText() + "4");
+                final_number = input.getText().toString() + "4";
+            }
+        });
+
+        btn_5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                input.setText(input.getText() + "5");
+                final_number = input.getText().toString() + "5";
+            }
+        });
+
+        btn_6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                input.setText(input.getText() + "6");
+                final_number = input.getText().toString() + "6";
+            }
+        });
+
+        btn_7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                input.setText(input.getText() + "7");
+                final_number = input.getText().toString() + "7";
+            }
+        });
+
+        btn_8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                input.setText(input.getText().toString() + "8");
+                final_number = input.getText().toString() + "8";
+            }
+        });
+
+        btn_9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                input.setText(input.getText().toString() + "9");
+                final_number = input.getText().toString() + "9";
+            }
+        });
+
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                input.setText(input.getText().toString() + "+");
+                Action = "add";
+                compute();
+                final_number = "";
+            }
+
+
+        });
+
+        btn_sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                input.setText(input.getText().toString() + "-");
+                Action = "sub";
+                compute();
+                final_number = "";
+
+            }
+
+
+
+        });
+
+        btn_reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                input.setText("");
+            }
+        });
+
+        btn_r.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                input.setText(final_result.toString());
+
+
+            }
+        });
 
 
 
 
     }
 
-    @Override
-    public void onClick(View view) {
+    public void compute() {
+        switch (Action){
+            case "add":{
+
+                final_result = final_result + Float.valueOf(final_number);
+            }
+
+            case "sub":{
+
+                final_result = final_result - Float.valueOf(final_number);
+            }
+        }
+
 
     }
 }
